cc754f6c1efdf42355efd79569287b9f23760fcd
Albert <albert.mramis@gmail.com>
2023-03-28 19:39:26 +0200

    codi per treballar

---
+++ registroUsuario.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * registroUsuario
+ */
+public class registroUsuario {
+
+  public static void main(String[] args) {
+    Scanner teclat = new Scanner(System.in);
+    String usuario;
+    String correo;
+    String password;
+    boolean correcto;
+
+    do {
+      System.out.println("introduce nombre");
+      usuario = teclat.next();
+      System.out.println("introduce correo");
+      correo = teclat.next();
+      System.out.println("introduce password");
+      password = teclat.next();
+      correcto = ValidarCampos.validarCampos(usuario, correo, password);
+    } while (!correcto);
+    System.out.println("usuario:" + usuario + " correo:" + correo + " password:" + password);
+    System.out.println("El registro se ha realizado con éxito");
+  }
+}
